f62c837788377d644485ce4a99fce7bb3be82ff0
root <xblaster@gmail.com>
2016-06-30 13:38:46 +0000

    pass to docker hub images

--- bash.sh
+++ bash.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v $1:/tf_files  tf bash
+docker run --rm -it -v $1:/tf_files  xblaster/tensor-guess bash
--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-docker run -v $1:/tf_files  tf
+docker run -v $1:/tf_files  xblaster/tensor-guess

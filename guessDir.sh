54c5c5c68cb949305a7b88acc6de52a18674118a
root <xblaster@gmail.com>
2016-08-01 12:42:57 +0000

    work now

--- guessDir.sh
+++ guessDir.sh
@@ -1,2 +1,2 @@
-#docker run -v $1:/tf_files -v $2:/toScan $3:/scanned xblaster/tensor-guess sh -c "python label_dir.py" 2> /dev/null
-docker run -v $1:/tf_files -v $2:/toScan $3:/scanned tf sh -c "python label_dir.py" 2> /dev/null
+docker run --rm -v $1:/tf_files -v $2:/toScan -v $3:/scanned xblaster/tensor-guess sh -c "python py/label_dir.py"
+#docker run --rm -it  -v $1:/tf_files -v $2:/toScan -v $3:/scanned tf bash
